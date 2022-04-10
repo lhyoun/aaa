6ed49461035b7978f2f19fb14c0b557b167f434f
lhy <gkdbsx@naver.com>
2022-04-11 03:33:46 +0900

    잠온다

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Pano from './component/Pano';
+import NavBarTest from './component/NavBarTest';
+import React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+
   const imageList = [
     {key: 1, image: '/collage.jpg', linkTo: 2, linkPositon: (-3429.01, 1205.85, -3421.88)},
     {key: 2, image: '/skyview01.jpg', linkTo: 1, linkPositon: (-3429.01, 1205.85, -3421.88)}
@@ -11,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <Pano imageList={imageList}/>
+      <NavBarTest imageList={imageList}></NavBarTest>
     </div>
   );
 }
--- src/component/NavBarTest.js
+++ src/component/NavBarTest.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Container, Navbar, NavBar } from 'react-bootstrap'
-const NavBarTest = () => {
+import { Carousel } from 'react-bootstrap'
+const NavBarTest = ({ imageList }) => {
 
 	return (
+		<>
+			<div style={{ position: 'absolute', width: '300px', bottom: '200px', left: '500px', backgroundColor: 'white', padding: '50px' }}>NavBarTest에서 네비게이션 만들기
+			<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/collage.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/skyview01.jpg"
+      alt="Second slide"
+    />
 
-		<div>dd</div>
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/collage.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+			</div>
+		</>
 		// <NavBar bg="light" expand="lg">
 		// 	<Container>
 		// 		<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
